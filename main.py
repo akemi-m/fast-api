586e5e664c17fb4eb30743456e010fcd00c1c885
akemi <akemi.mullis@gmail.com>
2025-05-19 09:46:55 -0300

    Atualização do endpoint get all posts

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Post(Base):
 
     id = Column(Integer, primary_key=True)
     title = Column(String(255))
-    created = Column(DateTime, default=datetime.utcnow)
+    created = Column(DateTime)
     subtitle = Column(String(122))
     authorid = Column(Integer, ForeignKey('author.id', ondelete='CASCADE'))
     author = relationship('Author')
@@ -70,49 +70,53 @@ def create_author(name: str, age: int):
         session.add(author)
         # dar commit
         session.commit()
+        return JSONResponse(content={'id': author.id, 'name': author.name, 'age': author.age}, status_code=201)
     except Exception as e:
         # evitar o commit pela metade
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
-
-    return JSONResponse(content={'id': author.id, 'name': author.name, 'age': author.age}, status_code=201)
 
 # @ é a property de uma classe - atributos que podem ou não ter valores default
 @app.post('/posts')
 # é uma função, não método, então não usa self
 def create_post(title: str, subtitle: str, authorid: int):
 
-    if not title:
-        return JSONResponse(content={'error': 'Title is required'}, status_code=400)
-    if not subtitle:
-        return JSONResponse(content={'error': 'Subtitle is required'}, status_code=400)
-    if not authorid:
-        return JSONResponse(content={'error': 'AuthorId is required'}, status_code=400)
-
   # manipulação do banco de dados - pode dar erro
     try:
-        post = Post(title=title, subtitle=subtitle, authorid=authorid)
+        if not title:
+            return JSONResponse(content={'error': 'Title is required'}, status_code=400)
+        if not subtitle:
+            return JSONResponse(content={'error': 'Subtitle is required'}, status_code=400)
+        if not authorid:
+            return JSONResponse(content={'error': 'AuthorId is required'}, status_code=400)
+        
+        author = session.query(Author).filter_by(id=authorid).first()
+        if not author:
+            return JSONResponse(content={'error': 'Author not found'}, status_code=404)
+        
+        post = Post(title=title, created=datetime.now(), subtitle=subtitle, authorid=authorid)
         # adição do objeto instanciado
         session.add(post)
         # dar commit
         session.commit()
+        return JSONResponse(content={'id': post.id, 'title': post.title, 'subtitle': post.subtitle, 'authorid': post.authorid}, status_code=201)
+
     except Exception as e:
         # evitar o commit pela metade
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
 
-    return JSONResponse(content={'id': post.id, 'title': post.title, 'subtitle': post.subtitle, 'authorid': post.authorid}, status_code=201)
-
 @app.put("/authors")
 def put_author(id: int, name: str, age: int):
-    if not id:
-        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
-    if not name:
-        return JSONResponse(content={'error': 'Name is required'}, status_code=400)
-    if not age:
-        return JSONResponse(content={'error': 'Age is required'}, status_code=400)
     
     try:
+        if not id:
+            return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+        if not name:
+            return JSONResponse(content={'error': 'Name is required'}, status_code=400)
+        if not age:
+            return JSONResponse(content={'error': 'Age is required'}, status_code=400)
+        
         # puxa do banco
         author = session.query(Author).filter_by(id=id).first()
         # verifica se não é nulo
@@ -122,25 +126,25 @@ def put_author(id: int, name: str, age: int):
         author.name = name
         author.age = age
         session.commit()
+        return JSONResponse(content={'id': author.id, 'name': author.name, 'age': author.age}, status_code=200)
+    
     except Exception as e:
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
 
-    return JSONResponse(content={'id': author.id, 'name': author.name, 'age': author.age}, status_code=200)
-
 @app.put("/posts")
-def put_post(id: int, title: str, subtitle: str, authorid: int):
-
-    if not id:
-        return JSONResponse(content={'error': 'Id is required'}, status_code=400)
-    if not title:
-        return JSONResponse(content={'error': 'Title is required'}, status_code=400)
-    if not subtitle:
-        return JSONResponse(content={'error': 'Subtitle is required'}, status_code=400)
-    if not authorid:
-        return JSONResponse(content={'error': 'AuthorId is required'}, status_code=400)
+def put_post(id: int, title: str, subtitle: str, authorid: str):
     
     try:
+        if not id:
+            return JSONResponse(content={'error': 'Id is required'}, status_code=400)
+        if not title:
+            return JSONResponse(content={'error': 'Title is required'}, status_code=400)
+        if not subtitle:
+            return JSONResponse(content={'error': 'Subtitle is required'}, status_code=400)
+        if not authorid:
+            return JSONResponse(content={'error': 'AuthorId is required'}, status_code=400)
+        
         # puxa do banco
         post = session.query(Post).filter_by(id=id).first()
         # verifica se não é nulo
@@ -156,45 +160,50 @@ def put_post(id: int, title: str, subtitle: str, authorid: int):
         post.subtitle = subtitle
         post.authorid = authorid
         session.commit()
+        return JSONResponse(content={'id': post.id, 'title': post.title, 'created': str(post.title),'subtitle': post.subtitle, 'idauthor': post.authorid}, status_code=200)
+    
     except Exception as e:
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
-
-    return JSONResponse(content={'id': post.id, 'title': post.title, 'subtitle': post.subtitle, 'idauthor': post.authorid}, status_code=200)
 
 @app.delete("/authors")
 def delete_author(id: int):
-    if not id:
-        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
     
     try:
+
+        if not id:
+            return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+        
         author = session.query(Author).filter_by(id=id).first()
         if not author:
             return JSONResponse(content={'error': 'Author not found'}, status_code=404)
+        
         session.delete(author)
         session.commit()
+        return JSONResponse(content={'message': 'Author deleted successfully'}, status_code=200)
+    
     except Exception as e:
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
-    
-    return JSONResponse(content={'message': 'Author deleted successfully'}, status_code=200)
 
 @app.delete("/posts")
 def delete_post(id: int):
-    if not id:
-        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
     
     try:
+        if not id:
+            return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+        
         post = session.query(Post).filter_by(id=id).first()
         if not post:
             return JSONResponse(content={'error': 'Post not found'}, status_code=404)
+        
         session.delete(post)
         session.commit()
+        return JSONResponse(content={'message': 'Post deleted successfully'}, status_code=200)
+    
     except Exception as e:
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
-    
-    return JSONResponse(content={'message': 'Post deleted successfully'}, status_code=200)
 
 @app.get('/authors/all')
 def get_all_authors():
@@ -205,63 +214,79 @@ def get_all_authors():
         authors = []
         for author in author_list:
             authors.append({'id':author.id, 'name': author.name, 'age': author.age})
+        return JSONResponse(content=authors, status_code=200)
+    
     except Exception as e:
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
-    
-    return JSONResponse(content=authors, status_code=200)
 
 @app.get('/posts/all')
 def get_all_posts():
     try:
-        post_list = session.query(Post).all()
-        if not post_list:
+        posts = session.query(Post).all()
+
+        if not posts:
             return JSONResponse(content={'error': 'No posts found'}, status_code=404)
-        posts = []
-        for post in post_list: # perguntar sobre created
-            posts.append({'id':post.id, 'title': post.title, 'subtitle': post.subtitle, 'authorid': post.authorid})
+
+        post_list = []
+        for post in posts:
+            
+            author = session.query(Author).filter_by(id=post.authorid).first()
+            if not author:
+                return JSONResponse(content={'error': 'Author not found'}, status_code=404)
+            
+            post_list.append({
+                'id': post.id,
+                'title': post.title,
+                'created': str(post.created),
+                'authorid': author.id,
+                'author_name': author.name
+            })
+        return JSONResponse(content=post_list, status_code=200)
+    
     except Exception as e:
         session.rollback()
-        return JSONResponse(content={'error': str(e)}, status_code=500)
-    
-    return JSONResponse(content=posts, status_code=200)
+        return JSONResponse(content={'error': str(e)}, status_code=500) 
 
 @app.get('/authors/{author_id}')
 def get_author(author_id: int):
-    if not author_id:
-        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
-    
+
     try:
+        if not author_id:
+            return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+    
         author = session.query(Author).filter_by(id=author_id).first()
         if not author:
             return JSONResponse(content={'error': 'Author not found'}, status_code=404)
+        return JSONResponse(content={'id': author.id, 'name': author.name, 'age': author.age}, status_code=200)
+    
     except Exception as e:
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
-    
-    return JSONResponse(content={'id': author.id, 'name': author.name, 'age': author.age}, status_code=200)
 
 @app.get('/posts/{post_id}')
 def get_post(post_id: int):
-    if not post_id:
-        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
-    
+
     try:
+        if not post_id:
+            return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+    
         post = session.query(Post).filter_by(id=post_id).first()
         if not post:
             return JSONResponse(content={'error': 'Post not found'}, status_code=404)
+        return JSONResponse(content={'id': post.id, 'title': post.title, 'created': str(post.created),'subtitle': post.subtitle, 'authorid': post.authorid}, status_code=200)
+
     except Exception as e:
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
     
-    return JSONResponse(content={'id': post.id, 'title': post.title, 'subtitle': post.subtitle, 'authorid': post.authorid}, status_code=200)
-
 @app.get('/authorsposts/{author_id}')
 def get_authorsposts(author_id: int):
-    if not author_id:
-        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
     
     try:
+        if not author_id:
+            return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+        
         author = session.query(Author).filter_by(id=author_id).first()
         if not author:
             return JSONResponse(content={'error': 'Author not found'}, status_code=404)
@@ -272,9 +297,9 @@ def get_authorsposts(author_id: int):
         
         posts = []
         for post in post_list: # perguntar sobre created
-            posts.append({'id':post.id, 'title': post.title, 'subtitle': post.subtitle})
+            posts.append({'id':post.id, 'title': post.title, 'created': str(post.created), 'subtitle': post.subtitle})
+
+        return JSONResponse(content=posts, status_code=200)
     except Exception as e:
         session.rollback()
         return JSONResponse(content={'error': str(e)}, status_code=500)
-    
-    return JSONResponse(content=posts, status_code=200)
